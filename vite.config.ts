f6c7bf71d346b66e0d2646b711f5f7103619cc69
gujihong <gujih@bsoft.com.cn>
2022-09-08 16:14:22 +0800

    refactor(init): file structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import {
+  createStyleImportPlugin,
+  NutuiResolve,
+} from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: "/",
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  plugins: [
+    vue(),
+    createStyleImportPlugin({
+      resolves: [NutuiResolve()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          "@import './src/styles/_var.scss';@import './src/styles/_mixins.scss';@import './src/styles/_common.scss';",
+      },
+    },
+  },
+  server: {
+    port: 9527,
+    proxy: {
+      "/api": {
+        target: "https://api.test.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
